6a1424623872f486e98d209c839b425eb50dad0a
Aayan Rizvi <74577196+Ulto85@users.noreply.github.com>
2023-10-10 11:03:50 -0700

    created app

---
+++ apcsapp/apcsapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  apcsapp
+//
+//  Created by Aayan Rizvi on 10/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ apcsapp/apcsapp/apcsappApp.swift
@@ -0,0 +1,17 @@
+//
+//  apcsappApp.swift
+//  apcsapp
+//
+//  Created by Aayan Rizvi on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct apcsappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
