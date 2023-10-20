64e6e2895f54bb2c9f5413af50211ba36f1b463d
Aayan Rizvi <74577196+Ulto85@users.noreply.github.com>
2023-10-20 01:14:06 -0700

    finished lol
    oop finished it

--- forrealthistime/forrealthistime/ViewController.swift
+++ forrealthistime/forrealthistime/ViewController.swift
@@ -26,6 +26,42 @@ class ViewController: UIViewController {
     
     
    
+ 
+    
+    
+    @IBOutlet var numero: UITextField!
+    
+    @IBOutlet var nombre: UITextField!
+    
+    @IBOutlet var connecion: UITextField!
+    
+    @IBAction func sendButtonPressed(_ sender: Any) {
+        let json: [String: Any] = ["number":numero.text,"name":nombre.text,"description":connecion.text]
+
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+
+        // create post request
+        let url = URL(string: "https://coolrepl.robowolf.repl.co/sendcall")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+
+        // insert json data to the request
+        request.httpBody = jsonData
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print(error?.localizedDescription ?? "No data")
+                return
+            }
+            let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+            if let responseJSON = responseJSON as? [String: Any] {
+                print(responseJSON)
+            }
+        }
+
+        task.resume()
+        
+    }
     
 
     
